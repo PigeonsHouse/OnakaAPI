236584bab098925565520300fd2d02ca2feef6f9
PigeonsHouse <bullbull1230@gmail.com>
2022-07-16 01:17:30 +0900

    [add] テーブルの定義とMigrate設定

--- db/db.go
+++ db/db.go
@@ -13,16 +13,14 @@ var (
 	Psql *gorm.DB
 )
 
-func InitDB() error {
-	var err error
-
+func InitDB() (err error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", utils.DbHost, utils.DbUser, utils.DbPass, utils.DbName, utils.DbPort)
 	Psql, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return err
+		return
 	}
-	if err = Psql.AutoMigrate(&User{}); err != nil {
-		return err
+	if err = Psql.AutoMigrate(&User{}, &Posts{}); err != nil {
+		return
 	}
-	return nil
+	return
 }
--- db/models.go
+++ db/models.go
@@ -1,8 +1,46 @@
 package db
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func (base *Base) BeforeCreate(tx *gorm.DB) (err error) {
+	base.ID = uuid.New().String()
+	return
+}
+
+type Base struct {
+	ID        string    `sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 type User struct {
-	ID           string `json:"id"`
+	Base
 	Name         string `json:"name"`
 	Email        string `json:"email"`
 	PasswordHash string `json:"password_hash"`
+}
+
+type Posts struct {
+	Base
+	UserID    string `json:"user_id"`
+	Content   string `json:"content"`
+	ImageUrl  string `json:"image_url"`
+	User      User   `gorm:"foreignKey:UserID;references:ID" json:"user"`
+	FunnyUser []User `gorm:"many2many:funnies;"`
+	YummyUser []User `gorm:"many2many:yummies;"`
+}
+
+type Funny struct {
+	UserID string `gorm:"primaryKey" json:"user_id"`
+	PostID string `gorm:"primaryKey" json:"post_id"`
+}
+
+type Yummy struct {
+	UserID string `gorm:"primaryKey" json:"user_id"`
+	PostID string `gorm:"primaryKey" json:"post_id"`
 }
