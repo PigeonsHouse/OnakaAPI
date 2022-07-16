62eba6a8f78838fc8ad825446d6574c253efac00
K-Kizuku <kotani.kizuku@gmail.com>
2022-07-17 01:04:26 +0900

    [delete] 不要な部分の削除

--- cruds/posts.go
+++ cruds/posts.go
@@ -60,33 +60,10 @@ func GetPost(postId string) (post db.Posts, err error) {
 
 }
 
-// func PostImages(file *multipart.FileHeader) (string, error){
-// 	temp := db.Psql.Create(&file)
-// 	cld, _ := cloudinary.NewFromParams(os.Getenv("CLOUD_NAME"), os.Getenv("API_KEY"), os.Getenv("API_SECRET"))
-// 	ctx := context.Background()
-// 	resp, err := cld.Upload.Upload(ctx, "apple.png", uploader.UploadParams{PublicID: "docs/sdk/go/apple",
-//     Transformation: "c_crop,g_center/q_auto/f_auto", Tags: []string{"fruit"}})
-// 	my_image, err := cld.Image("docs/sdk/go/apple")
-// 	if err != nil {
-// 		fmt.Println("error")
-// 	}
-// 	url, err := my_image.String()
-// 	if err != nil {
-// 		fmt.Println("error")
-// 	}
-
-// 	// temp := db.Psql.Create(&file)
-// 	// n, err := rand.Int(rand.Reader, big.NewInt(100))
-// 	// url := "./static/"+ n.String() + "_"+ time.Now().String() +".png"
-// 	// u := &url.URL{}
-// 	// f, _ := os.Create(url)
-	
-// }
 
 func PostPosts(content string, url string, userId string) (db.Posts, error){
 	p := db.Posts{Content: content, ImageUrl: url, UserID: userId}
 	db.Psql.Create(&p)
-	//res_posts := db.Posts{ID: p.ID, Content: p.Content, ImageUrl: p.ImageUrl, CreatedAt: p.CreatedAt, UpdatedAt: p.UpdatedAt}
 	if err := db.Psql.First(&p, "id = ?", p.ID).Error; err != nil{
 		return p, err
 	}
