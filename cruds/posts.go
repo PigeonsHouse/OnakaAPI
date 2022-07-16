72b344c042ce739ad842993548b77c0f3e1be313
PigeonsHouse <bullbull1230@gmail.com>
2022-07-16 17:45:50 +0900

    [add] postの詳細取得エンドポイントの作成

--- cruds/posts.go
+++ cruds/posts.go
@@ -30,3 +30,27 @@ func GetTimeLine() (timeline []db.Posts, err error) {
 
 	return
 }
+
+func GetPost(postId string) (post db.Posts, err error) {
+	err = db.Psql.First(&post, "id = ?", postId).Error
+	if err != nil {
+		return
+	}
+	var user []db.User
+	err = db.Psql.Model(&post).Association("User").Find(&user)
+	if err != nil {
+		return
+	}
+	post.User = user[0]
+	err = db.Psql.Model(&post).Association("FunnyUsers").Find(&user)
+	if err != nil {
+		return
+	}
+	post.FunnyUsers = user
+	err = db.Psql.Model(&post).Association("YummyUsers").Find(&user)
+	if err != nil {
+		return
+	}
+	post.YummyUsers = user
+	return
+}
