9ecb7714604c594f560f0dff0016912d1752be59
PigeonsHouse <bullbull1230@gmail.com>
2022-07-17 00:36:21 +0900

    [add] postの削除エンドポイント作成

--- routers/posts.go
+++ routers/posts.go
@@ -11,9 +11,16 @@ func initPostRouter(pr *gin.RouterGroup) {
 	pr.Use(middleware)
 	pr.GET("", getPosts)
 	pr.GET("/:post_id", getPostById)
+	pr.DELETE("/:post_id", deletePost)
 }
 
 func getPosts(c *gin.Context) {
+	if _, isExist := c.Get("user_id"); !isExist {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "token is invalid",
+		})
+		return
+	}
 	timeline, err := cruds.GetTimeLine()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -26,6 +33,12 @@ func getPosts(c *gin.Context) {
 }
 
 func getPostById(c *gin.Context) {
+	if _, isExist := c.Get("user_id"); !isExist {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "token is invalid",
+		})
+		return
+	}
 	postId := c.Param("post_id")
 	post, err := cruds.GetPost(postId)
 	if err != nil {
@@ -35,4 +48,29 @@ func getPostById(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, post)
+}
+
+func deletePost(c *gin.Context) {
+	var (
+		userId  any
+		isExist bool
+	)
+	if userId, isExist = c.Get("user_id"); !isExist {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "token is invalid",
+		})
+		return
+	}
+
+	postId := c.Param("post_id")
+	err := cruds.DeletePost(postId, userId.(string))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
 }
